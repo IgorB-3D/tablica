ea1be35090e81158412d8468049765df1781c6d6
IgorB-3D <90689808+IgorB-3D@users.noreply.github.com>
2021-11-16 12:20:18 +0100

    Added card construction

--- script/app.js
+++ script/app.js
@@ -1,2 +1,26 @@
 const root = document.getElementById('app')
+const columns = document.querySelectorAll('.cardcolumn')
 
+/*
+<div class="panel block border darkfill smallpad">
+<p class="bold med marginclear spacetop">Karta</p>
+<p class="sml card marginclear spacetop">Opis</p>
+</div>
+*/
+
+function newCard(before) {
+    let card = document.createElement("div");
+    card.classList.add("panel", "block", "border", "darkfill", "smallpad", "spacetop")
+
+    let cardTitle = document.createElement("p")
+    cardTitle.classList.add("bold", "med", "marginclear", "spacetop")
+    cardTitle.innerHTML = "Nowa karta"
+    card.appendChild(cardTitle)
+
+    let cardText = document.createElement("p")
+    cardText.classList.add("sml", "card", "marginclear", "spacetop")
+    cardText.innerHTML = "Opis nowej karty"
+    card.appendChild(cardText)
+
+    before.parentNode.prepend(card)
+}
