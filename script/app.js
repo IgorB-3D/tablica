3c970c530fac64eee7bc4590446090aabfe30d6c
IgorB-3D <90689808+IgorB-3D@users.noreply.github.com>
2021-11-16 13:20:58 +0100

    Made cards draggable

--- script/app.js
+++ script/app.js
@@ -9,6 +9,42 @@ const pages = document.querySelectorAll('.page')
 </div>
 */
 
+const drag = {
+    card: null,
+    mPos: null,
+    mDelta: null
+}
+
+function dragBegin(card) {
+    drag.card = card
+    drag.mDelta = {
+        x: drag.mPos.x - card.getBoundingClientRect().left,
+        y: drag.mPos.y - card.getBoundingClientRect().top
+    }
+    card.style.position = 'absolute'
+    dragUpdate()
+}
+
+function dragEnd(card) {
+    card.style.position = 'initial'
+    drag.card = null
+}
+
+function dragUpdate(mv) {
+    if (drag.card == null)
+        return;
+    drag.card.style.top = `${drag.mPos.y - drag.mDelta.y}px`
+    drag.card.style.left = `${drag.mPos.x - drag.mDelta.x}px`
+}
+
+document.addEventListener('mousemove', (ev) => {
+    drag.mPos = {
+        x: ev.x,
+        y: ev.y
+    }
+    dragUpdate()
+})
+
 function newProject() {
     pages[0].classList.add('hide')
     pages[1].classList.remove('hide')
@@ -19,14 +55,17 @@ function newCard(before) {
     card.classList.add("panel", "block", "border", "darkfill", "smallpad", "spacetop")
 
     let cardTitle = document.createElement("input")
-    cardTitle.classList.add("clear", "white", "bold", "med", "marginclear", "spacetop")
+    cardTitle.classList.add("clear", "white", "bold", "med", "marginclear", "spacetop", "block")
     cardTitle.value = "Nowa karta"
     card.appendChild(cardTitle)
 
-    let cardText = document.createElement("p")
-    cardText.classList.add("sml", "card", "marginclear", "spacetop")
+    let cardText = document.createElement("textarea")
+    cardText.classList.add("sml", "card", "marginclear", "spacetop", "fullwidth", "clear", "font", "white")
     cardText.innerHTML = "Opis nowej karty"
     card.appendChild(cardText)
+
+    card.addEventListener('mousedown', () => dragBegin(card))
+    document.addEventListener('mouseup', () => dragEnd(card))
 
     before.parentNode.prepend(card)
 }
