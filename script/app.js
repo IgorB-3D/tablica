84c9fe622274c83c3001a939460e851402be3dd0
Abcight <abcightdev@gmail.com>
2021-11-28 16:24:56 +0100

    Added github integration

--- script/app.js
+++ script/app.js
@@ -23,7 +23,7 @@ function reconstruct()
 	}
 
 	for(let i = 0; i < data.cards.length; i++)
-		newCard(null, data.cards[i])
+		Card.CreateElement(null, data.cards[i])
 }
 
 function getColumnIdx(column)
@@ -62,73 +62,15 @@ function hidePanel()
     pages[1].classList.remove('in')
 }
 
-function newCard(before, def) {
-	let index = -1
+function setShaLen(commitText)
+{
+	let width = ''
 
-	if(!def)
-	{
-		def = new Card()
-		def.columnIndex = getColumnIdx(before.parentNode)
-	
-		let index = data.cards.length
-		data.cards[index] = def
-	}
+	if(commitText.value.length != 0)
+		width = `min(${commitText.value.length + 2}ch, 50%)`
 	else
-		before = columns[def.columnIndex].childNodes[0]
-	
-	for(let i = 0; i < data.cards.length; i++)
-		if(data.cards[index] == def)
-			index = i
+		width = '13ch'
 
-    let card = document.createElement("div");
-    card.classList.add("panel", "block", "border", "darkfill", "smallpad", "spacetop")
-
-    let cardTitle = document.createElement("input")
-    cardTitle.classList.add("clear", "white", "bold", "med", "marginclear", "spacetop", "block")
-    cardTitle.value = def.title
-	cardTitle.addEventListener('change', () => {
-		data.cards.forEach(x => {
-			if(x == def)
-				x.title = cardTitle.value
-		})
-		data.Flush()
-	})
-    card.appendChild(cardTitle)
-
-    let cardText = document.createElement("textarea")
-    cardText.classList.add("sml", "card", "marginclear", "spacetop", "fullwidth", "clear", "font", "white")
-    cardText.innerHTML = def.description
-	cardText.addEventListener('change', () => {
-		data.cards.forEach(x => {
-			if(x == def)
-				x.description = cardText.value
-		})
-		data.Flush()
-	})
-    card.appendChild(cardText)
-
-	let commitText = document.createElement("input")
-	commitText.classList.add("gitbg", "clear", "white", "marginclear", "block")
-	commitText.placeholder = 'Commit SHA'
-	commitText.value = def.sha
-	commitText.addEventListener('change', () => {
-		data.cards.forEach(x => {
-			if(x == def)
-				x.sha = commitText.value
-		})
-		data.Flush()
-	})
-	card.appendChild(commitText)
-
-    card.addEventListener('mousedown', (e) => {
-		if(e.target == card)
-			drag.Begin(card, def)
-	})
-    document.addEventListener('mouseup', () => drag.End(card))
-
-    before.parentNode.prepend(card)
-
-	data.Flush()
-
-	return card
+	commitText.style.width = width
+	commitText.style.maxWidth = width
 }
--- script/card.js
+++ script/card.js
@@ -7,4 +7,104 @@ class Card
 		this.sha = ''
 		this.columnIndex = 0
 	}
+
+	static CreateElement(before, def)
+	{
+		let index = -1
+
+		if(!def)
+		{
+			def = new Card()
+			def.columnIndex = getColumnIdx(before.parentNode)
+		
+			let index = data.cards.length
+			data.cards[index] = def
+		}
+		else
+			before = columns[def.columnIndex].childNodes[0]
+		
+		for(let i = 0; i < data.cards.length; i++)
+			if(data.cards[index] == def)
+				index = i
+
+		let card = document.createElement("div");
+		card.classList.add("panel", "block", "border", "darkfill", "smallpad", "spacetop", 'crd')
+
+		let cardTitle = document.createElement("input")
+		cardTitle.classList.add("clear", "white", "bold", "med", "marginclear", "spacetop", "block")
+		cardTitle.value = def.title
+		cardTitle.addEventListener('change', () => {
+			data.cards.forEach(x => {
+				if(x == def)
+					x.title = cardTitle.value
+			})
+			data.Flush()
+		})
+		card.appendChild(cardTitle)
+
+		let cardText = document.createElement("textarea")
+		cardText.classList.add("sml", "card", "marginclear", "spacetop", "fullwidth", "clear", "font", "white")
+		cardText.innerHTML = def.description
+		cardText.addEventListener('change', () => {
+			data.cards.forEach(x => {
+				if(x == def)
+					x.description = cardText.value
+			})
+			data.Flush()
+		})
+		card.appendChild(cardText)
+
+		let commitWrapper = document.createElement('div')
+		commitWrapper.classList.add('commitwrapper')
+		card.appendChild(commitWrapper)
+
+		let commitUrl = document.createElement('a')
+		commitUrl.classList.add('inline', 'white', 'gitcom')
+		commitUrl.href = ''
+		commitUrl.innerHTML = ''
+
+		let commitText = document.createElement("input")
+		commitText.classList.add("gitbg", "clear", "white", "marginclear", 'inline')
+		commitText.placeholder = 'Commit SHA'
+		commitText.value = def.sha
+		setShaLen(commitText)
+		commitText.addEventListener('input', () => {
+			setShaLen(commitText)
+			data.cards.forEach(x => {
+				if(x == def)
+					x.sha = commitText.value
+			})
+			data.Flush()
+		})
+
+		commitText.addEventListener('change', () => {
+			GetCommit(data.repo, commitText.value, (res) => {
+				commitUrl.href = res["html_url"]
+				commitUrl.innerText = res["commit"]["message"].replace(/(\r\n|\n|\r)/gm, "")
+			})
+		})
+
+		if(commitText.value && commitText.value.length != 0)
+		{
+			GetCommit(data.repo, commitText.value, (res) => {
+				commitUrl.href = res["html_url"]
+				commitUrl.innerText = res["commit"]["message"].replace(/(\r\n|\n|\r)/gm, "")
+			})
+		}
+
+		commitWrapper.appendChild(commitText)
+		commitWrapper.appendChild(commitUrl)
+
+		card.addEventListener('mousedown', (e) => {
+			if(e.target == card && e.button == 0)
+				drag.Begin(card, def)
+		})
+		document.addEventListener('mouseup', () => drag.End(card))
+
+		before.parentNode.prepend(card)
+
+		data.Flush()
+
+		return card
+	}
 }
